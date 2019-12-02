9d04b3264f8fb05c1ce53ff48fb4673350de45e3
singhsays <singhsays@gmail.com>
2019-12-02 02:42:21 -0800

    Add unit tests for when the message is not a string

--- tests/test.py
+++ tests/test.py
@@ -19,11 +19,15 @@ from unittest.mock import patch
 class TestClarity(unittest.TestCase):
     """Unit tests for clarity logger."""
 
-    def run_test(self, method: str, color: str) -> None:
+    def run_test(self, method: str, color: str, use_str: bool = True) -> None:
         with patch("clarity.logging.{}".format(method)) as mocked_method:
             message = "test {} message".format(method)
-            expected = color + message + style.RESET
-            getattr(log, method)(message)
+            if use_str:
+                expected = color + message + style.RESET
+                getattr(log, method)(message)
+            else:
+                expected = color + "{{'msg': '{}'}}".format(message) + style.RESET
+                getattr(log, method)({"msg": message})
             mocked_method.assert_called_with(expected)
 
     def test_all_methods(self) -> None:
@@ -37,6 +41,18 @@ class TestClarity(unittest.TestCase):
         }
         for level, color in cases.items():
             self.run_test(level, color)
+
+    def test_non_str_messages(self) -> None:
+        cases: Dict[str, str] = {
+            "exception": fore.ORANGE_1,
+            "fatal": fore.RED,
+            "error": fore.LIGHT_RED,
+            "warning": fore.YELLOW,
+            "info": fore.DARK_GRAY,
+            "debug": fore.STEEL_BLUE,
+        }
+        for level, color in cases.items():
+            self.run_test(level, color, use_str=False)
 
 
 def suite():
